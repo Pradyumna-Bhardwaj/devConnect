a1fda4eb38b48dd61218cd5d2f883a345c7b20b3
Pradyumna Bhardwaj <pradyumna@Pradyumnas-MacBook-Pro.local>
2026-03-14 22:55:18 +0530

    multiple route handlers

--- src/app.js
+++ src/app.js
@@ -1,11 +1,20 @@
 const express = require("express");
 
-const app = expres();
+const app = express();
+
+app.use("/hello",(req, res,next)=>{
+    console.log("hello middleware 1");
+    next();
+},
+(req,res,next)=>{
+    console.log("hello middleware 2");
+    res.send("hello middleware 2");
+})
+
+
+
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
 });
 
-app.use(()=>{
-    
-})
