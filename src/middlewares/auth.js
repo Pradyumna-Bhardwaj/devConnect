c8d5cc1149d214c0c11f01f93d90c92e19de4eae
Pradyumna Bhardwaj <pradyumna@Pradyumnas-MacBook-Pro.local>
2026-03-28 19:40:54 +0530

    feed api added

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,11 +1,11 @@
 const jwt = require("jsonwebtoken");
-const User = require("../models/user")
+const User = require("../models/user");
 
 const userAuth = async (req, res, next) => {
   //read the cookie
   try {
     const { token } = req.cookies;
-    if(!token){
+    if (!token) {
       throw new Error("Invalid token, login again");
     }
 
@@ -16,15 +16,15 @@ const userAuth = async (req, res, next) => {
 
     //find the user
     const user = await User.findById(decoded._id);
-    if(!user){
+    if (!user) {
       throw new Error("User not found");
     }
+    
     req.user = user;
     next();
   } catch (err) {
     return res.status(400).send("Some error occurred: " + err.message);
   }
-
 };
 
 module.exports = { userAuth };
--- src/routes/user.js
+++ src/routes/user.js
@@ -2,7 +2,9 @@ const express = require("express");
 const userRouter = express.Router();
 const { userAuth } = require("../middlewares/auth");
 const ConnectionRequest = require("../models/connectionRequest");
+const User = require("../models/user");
 
+// get all connection requests received by the logged in user
 userRouter.get("/user/request/received", userAuth, async (req, res) => {
   try {
     const receivedRequests = await ConnectionRequest.find({
@@ -15,6 +17,7 @@ userRouter.get("/user/request/received", userAuth, async (req, res) => {
   }
 });
 
+// get all connections of the logged in user
 userRouter.get("/user/connections", userAuth, async (req, res) => {
   try {
     const connections = await ConnectionRequest.find({
@@ -26,7 +29,6 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
       .populate("fromUserId", "firstName lastName")
       .populate("toUserId", "firstName lastName");
 
-
     // Implementing if connection req. was sent by the logged in user then connection sould show toUserId else fromUserId data
     data = connections.map((connection) => {
       if (connection.fromUserId._id.equals(req.user._id)) {
@@ -37,6 +39,37 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
     });
 
     res.status(200).json({ data });
+  } catch (err) {
+    res.status(400).send("Some error occurred: " + err.message);
+  }
+});
+
+//get feed for the logged in user
+userRouter.get("/feed", userAuth, async (req, res) => {
+  try {
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 10;
+    limit > 50 ? (limit = 50) : limit;
+    const skip = (page - 1) * limit;
+    //TODO: find all requests linked with logged in user.
+    const userRequests = await ConnectionRequest.find({
+      $or: [{ fromUserId: req.user._id }, { toUserId: req.user._id }],
+    });
+    //TODO: make a set of the user ids
+    const hiddenUsersIds = new Set();
+    userRequests.forEach((req) => {
+      hiddenUsersIds.add(req.fromUserId);
+      hiddenUsersIds.add(req.toUserId);
+    });
+    //TODO: make a user ids set excluding the above ids.
+    const feedUsers = await User.find({
+      _id: { $nin: Array.from(hiddenUsersIds) },
+    })
+      .select("firstName lastName phtotoUrl skills")
+      .skip(skip)
+      .limit(limit);
+
+    res.status(200).json({ feedUsers });
   } catch (err) {
     res.status(400).send("Some error occurred: " + err.message);
   }
